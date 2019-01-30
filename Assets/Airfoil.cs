a66da990e69c0eaf8f7a66588a80bcef151fb38b
zgoddard3 <zgoddard3@gatech.edu>
2019-01-30 10:25:36 -0500

    First commit

---
+++ Assets/Airfoil.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Airfoil
+{
+	private float span = 2;
+	private float cord = 0.5f;
+	private float AR;
+	private float cl0 = 0.1f;
+	private float cd0 = 0.01f;
+	private float rho = 1.225f;
+    public Airfoil(){
+        
+    }
+}
---
+++ Assets/turtle.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turtle : MonoBehaviour
+{
+    private CharacterController controller;
+    public float thrust;
+    public float torque;
+
+    private float speed;
+    private float angular_rate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speed = 0;
+        angular_rate = 0;
+        if (Input.GetKey(KeyCode.W)) {
+            speed += thrust;
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            speed -= thrust;
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            angular_rate -= torque;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            angular_rate += torque;
+        }
+
+        transform.Rotate(0, angular_rate, 0);
+        controller.SimpleMove(transform.forward * speed);
+    }
+}
